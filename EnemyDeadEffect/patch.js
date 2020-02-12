07aaae4313ba7069f56467c2373dd482c1cd503a
bttf <design0822@naver.com>
2020-02-13 00:07:33 +0900

    Taking First Step!

---
+++ EnemyDeadEffect/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "EnemyDeadEffect",
+	"servers": [
+		"https://raw.githubusercontent.com/bttf0504/thmod/master/EnemyDeadEffect/"
+	],
+	"title": "(Th17)When enemy dies, the effect that appears when boss dies appear."
+}
---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+	"contact": "design0822@naver.com",
+	"id": "BTTF",
+	"patches": {
+		"EnemyDeadEffect": "(Th17)When enemy dies, the effect that appears when boss dies appear."
+	},
+	"servers": [
+		"https://raw.githubusercontent.com/bttf0504/thmod/master/"
+	],
+	"title": "BTTF's Patch Repo"
+}
